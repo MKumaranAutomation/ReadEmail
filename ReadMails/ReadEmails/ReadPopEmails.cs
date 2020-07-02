97ccc3a40542bfbeb7a6b421edb484f47364288d
Muthukumaran M <muthukumaran.m@justanswer.com>
2020-07-03 04:12:08 +0530

    Added new class for reading emails

---
+++ ReadMails/ReadEmails/ReadPopEmails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MailKit.Net.Pop3;
+using MailKit.Security;
+
+namespace ReadEmails
+{
+	class PrReadPopEmailsogram
+	{
+		public static void Main(string[] args)
+		{
+			using (var client = new Pop3Client())
+			{
+				client.Connect("pop.gmail.com", 995, SecureSocketOptions.SslOnConnect);
+
+				client.Authenticate("emailId", "Password");
+				for (int i = 0; i < client.Count; i++)
+				{
+					var message = client.GetMessage(i);
+					Console.WriteLine("Subject: {0}", message.Subject);
+				}
+
+				client.Disconnect(true);
+			}
+		}
+	}
+}
